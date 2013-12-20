9676e4d2c5fff858bf72ff31fd7399eb0f356736
Paul Hammond <paul.hammond@bbc.co.uk>
2013-12-20 13:28:13 +0000

    added a delinquo factory to count number of overall failures and attach to the model - will be used for forecast filter

--- app/scripts/services/delinquo.js
+++ app/scripts/services/delinquo.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('delinquoFrontendApp')
-  .service('delinquo', function delinquo($http, CONFIG) {
+  .service('delinquo', function delinquo($http, CONFIG, delinquoFactory) {
 
     this.getProject = function (projectName, success, error) {
       $http.get(CONFIG.delinquoBaseUrl + '/project/' + projectName).then(
         function (response) {
-          success(response.data);
+          success(new delinquoFactory(response.data));
         },
         function (response){
           error();
